4e0c65b8b4bba5d96dfa57dcd80c59077dbc0f39
René Olšák <rene.olsak@email.cz>
2023-01-11 11:01:32 +0100

    Basic login design without functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import './App.scss'
+import Signup from './components/Signup'
 
 
 function App() {
 
   return (
     <div className="app-container">
-      
+      <Signup />
     </div>
   )
 }
